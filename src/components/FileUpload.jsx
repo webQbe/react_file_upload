d30f6fa52cb11df9caa2b0efa1314dc1c95bfe87
webQbe <webcubesolutionslk@gmail.com>
2025-02-10 14:09:35 +0530

    Updated File and Filename States on Inputchange

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,12 +1,27 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const FileUpload = () => {
+  // Set File State
+  const [file, setFile] = useState('');
+  // Set Filename State
+  const [filename, setFilename] = useState('Choose File');
+
+  // Handle Input Change Event
+  const onChange = e => {
+    setFile(e.target.files[0]); // Get file object
+    setFilename(e.target.files[0].name); // Get file name
+  }
+
+
   return (
     <Fragment>
         <form>
             <div className="custom-file mb-4">
-                <input type="file" className="custom-file-input" id="customFile" />
-                <label htmlFor="customFile" className="custom-file-label"></label>
+                <input type="file" className="custom-file-input" id="customFile" 
+                onChange={onChange}/>
+                <label htmlFor="customFile" className="custom-file-label">
+                    {filename}
+                </label>
             </div>
             <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
         </form>
